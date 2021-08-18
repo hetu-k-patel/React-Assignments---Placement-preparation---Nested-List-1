920bd8f7a989b922fe4357476fe97c40ea839e5c
Sai Kumar <56537473+kumarsai131@users.noreply.github.com>
2021-08-18 15:48:46 +0530

    Create City.js

---
+++ src/components/City.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+const City = ({name,towns,index}) =>{
+  const[click,setClick] = useState(false)
+  return(
+    <li>
+      <h3 id={"city"+(index+1)} onClick = {setClick(!click)}>{name}</h3>
+      { click ? (
+        <ul>
+          {towns.map((e)=>(
+            <Town name = {e.name} index = {towns.indexOf(e)} />
+          ))}
+        </ul>
+      ) : null }
+    </li>
+  )
+}
+
+export default City;
